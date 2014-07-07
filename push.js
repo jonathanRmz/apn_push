a414b38d9a236b9a4d647c6bc2182595bef288f8
Jonathan Ramírez <rjonathan89@gmail.com>
2014-07-07 14:37:27 +0200

    modified

--- push.js
+++ push.js
@@ -1,22 +1,24 @@
+//module APN
 var apn = require('apn');
 
+//Options to the APN
 var options = {
-		cert:"",
-		key:"",
-		passphrase:"okm095IJN",
+		cert:"cert.pem",
+		key:"key.pem",
+		passphrase:"mypass",
 		production: false
 };
 
 var apnConnection = new apn.Connection(options);
 
-var myDevice = new apn.Device(token);
+var myDevice = new apn.Device("tokenDevice"); 
 
 var note = new apn.Notification();
 
 note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
 note.badge = 3;
-note.sound = "ping.aiff";
+note.sound = "Default";
 note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
-note.payload = {'messageFrom': 'Caroline'};
+note.payload = {'messageFrom': 'MyApp'};
 
 apnConnection.pushNotification(note, myDevice);
